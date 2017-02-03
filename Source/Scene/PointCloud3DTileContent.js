2cfb165e22ecdde450cad62f34b29f9e90fcd8f8
Sean Lilley <lilleyse@gmail.com>
2017-02-03 15:50:33 -0500

    Fix issue where transparency is ignored when any style is applied

--- Source/Scene/PointCloud3DTileContent.js
+++ Source/Scene/PointCloud3DTileContent.js
@@ -847,7 +847,10 @@ define([
             colorStyleFunction = style.getColorShaderFunction('getColorFromStyle', 'czm_tiles3d_style_', shaderState);
             showStyleFunction = style.getShowShaderFunction('getShowFromStyle', 'czm_tiles3d_style_', shaderState);
             pointSizeStyleFunction = style.getPointSizeShaderFunction('getPointSizeFromStyle', 'czm_tiles3d_style_', shaderState);
-            styleTranslucent = shaderState.translucent;
+
+            if (defined(colorStyleFunction) && !shaderState.translucent) {
+                styleTranslucent = false;
+            }
         }
 
         content._styleTranslucent = styleTranslucent;
